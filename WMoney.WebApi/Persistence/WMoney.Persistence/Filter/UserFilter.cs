978102a930a4dff9012c50ba708ed38f2fd72c55
weberson <weberson1990@gmail.com>
2015-03-14 16:20:08 -0300

    Created UserCore

---
+++ WMoney.WebApi/Persistence/WMoney.Persistence/Filter/UserFilter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMoney.Persistence.Model;
+
+namespace WMoney.Persistence.Filter
+{
+    public class UserFilter
+    {
+        //public Task<User> GetByEmail(this UserRepository user, string )
+        //{ 
+        
+        //}
+    }
+}
---
+++ WMoney.WebApi/WMoney.Core/UserCore.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WMoney.Persistence.Model;
+
+namespace WMoney.Core
+{
+    public class UserCore : IUserCore
+    {
+        public Task<User> CreateUserAsync(string email, string password)
+        { 
+            
+        }
+    }
+}
