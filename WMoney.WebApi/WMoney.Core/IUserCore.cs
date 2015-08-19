4e06aaebe575f357b031b7289e02ef2f71f31beb
weberson <weberson1990@gmail.com>
2015-08-19 20:31:37 -0300

    Implemented create user on user controller

--- WMoney.WebApi/WMoney.Core/IUserCore.cs
+++ WMoney.WebApi/WMoney.Core/IUserCore.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WMoney.Persistence.Model;
 
 namespace WMoney.Core
 {
     public interface IUserCore
     {
+        Task<User> CreateUserAsync(string email, string password);
     }
 }
